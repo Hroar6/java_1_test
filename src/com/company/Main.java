4e53284484c358a1c7f96ee5909409c05dd254e1
Hroar6 <romaartemyev@gmail.com>
2020-08-31 22:54:38 +0300

    homework 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,135 +1,214 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    static final int SIZE = 3;
+    static final int DOT_TO_WIN = 3;
+
+    static final char DOT_X = 'X';
+    static final char DOT_O = 'O';
+    static final char DOT_EMPTY = '.';
+
+    static char[][] map;
+
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
 
     public static void main(String[] args) {
+        initMap();
+        printMap();
 
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Вы победили! Поздравляем!");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
 
-        //1
-        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] == 0) {
-                arr1[i] = 1;
-            } else {
-                arr1[i] = 0;
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Компьютер победил.");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
             }
         }
-        System.out.println(Arrays.toString(arr1));
-
-        //2
-        int[] arr2 = new int[8];
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = i * 3;
-        }
-        System.out.println(Arrays.toString(arr2));
-
-        //3
-        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr3.length; i++) {
-            if (arr3[i] < 6) {
-                arr3[i] *= 2;
-            }
-        }
-        System.out.println(Arrays.toString(arr3));
-
-        //4
-        int[][] arr4 = new int[7][7];
-        for (int i = 0; i < arr4.length; i++) {
-            arr4[i][i] = 1;
-            arr4[i][arr4.length - 1 - i] = 1;
-        }
-        printArr2d(arr4);
-
-
-        //5
-        int[] arr5 = {4, 23, 10, 7, 41};
-        System.out.println("min = " + minArr(arr5));
-        System.out.println("max = " + maxArr(arr5));
-
-        //6
-        int[] arr6 = {1, 2, 3, 4, 5, 6, 7, 14};
-        System.out.println(sumPartArr(arr6));
-        System.out.println(Arrays.toString(arr6));
-
-        //7
-        System.out.println(Arrays.toString(shiftArr(arr6, 9)));
-        //  System.out.println(-1%5);
 
     }
 
-    // Вывод 2-мерного массива
-    static void printArr2d(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.printf("%3d", arr[i][j]);
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        System.out.print("  ");
+        for (int i = 1; i <= map.length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < map.length; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.printf("%c ", map[i][j]);
             }
             System.out.println();
         }
     }
 
-    static int minArr(int[] arr) {
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
+    public static void humanTurn() {
+        int x;
+        int y;
+
+        do {
+            System.out.println("input X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_X;
     }
 
-    static int maxArr(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+    public static boolean isCellValid(int y, int x) {
+        if (y < 0 || x < 0 || y >= SIZE || x >= SIZE) {
+            return false;
         }
-        return max;
+        return map[y][x] == DOT_EMPTY;
     }
 
-    //6
-    static boolean sumPartArr(int[] arr) {
-        int leftSide = arr[0];
-        int rightSide = arr[arr.length - 1];
-        int k; // индекс для rightSide
-        k = arr.length - 1; //
-        for (int i = 1; i < k; i++) {
-            if (leftSide < rightSide) {
-                leftSide += arr[i];
-            } else {
-                rightSide += arr[k - 1];
-                k--;
-                i--;
-            }
-        }
-        return leftSide == rightSide;
-    }
+    public static void aiTurn() {
+        int x;
+        int y;
+        //  boolean turnDone = false;
 
-    //7
-    static int[] shiftArr(int[] arr, int n) {
-        int length = arr.length;
-        n = n % length;
-        if (n >= 0) {
-            for (int i = 0; i < n; i++) {
-                int last = arr[length - 1];
-                for (int j = length - 1; j > 0; j--) {
-                    arr[j] = arr[j - 1];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                // Смотрит можно ли победить
+                if (map[i][j] == DOT_EMPTY) {
+                    map[i][j] = DOT_O;
+                    if (checkWin(DOT_O)) {
+                        return;
+                    } else {
+                        map[i][j] = DOT_EMPTY;
+                    }
                 }
-                arr[0] = last;
-            }
-        } else {
-            n = -n;
-            for (int i = 0; i < n; i++) {
-                int first = arr[0];
-                for (int j = 0; j < length - 1; j++) {
-                    arr[j] = arr[j + 1];
+                // Смотрит может ли победить противник
+                if (map[i][j] == DOT_EMPTY) {
+                    map[i][j] = DOT_X;
+                    if (checkWin(DOT_X)) {
+                        map[i][j] = DOT_O;
+                        //                turnDone = true;
+                        return;
+                    } else {
+                        map[i][j] = DOT_EMPTY;
+                    }
                 }
-                arr[length - 1] = first;
             }
         }
-        return arr;
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_O;
     }
 
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkWin(char c) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (chekX(i, j, c) || chekY(i, j, c) || chekDiagonal1(i, j, c) || chekDiagonal2(i, j, c)) {
+                    return true;
+                }
+            }
+        }
+       /* if (map[0][0] == c && map[0][1] == c && map[0][2] == c) {return true; }
+        if (map[1][0] == c && map[1][1] == c && map[1][2] == c) {return true; }
+        if (map[2][0] == c && map[2][1] == c && map[2][2] == c) {return true; }
+
+        if (map[0][0] == c && map[1][0] == c && map[2][0] == c) {return true; }
+        if (map[0][1] == c && map[1][1] == c && map[2][1] == c) {return true; }
+        if (map[0][2] == c && map[1][2] == c && map[2][2] == c) {return true; }
+
+        if (map[0][0] == c && map[1][1] == c && map[2][2] == c) {return true; }
+        if (map[0][2] == c && map[1][1] == c && map[2][0] == c) {return true; }*/
+
+        return false;
+    }
+
+    static boolean chekY(int i, int j, char c) {
+        if (j + DOT_TO_WIN - 1 < SIZE) {
+            for (int k = j; k < j + DOT_TO_WIN; k++) {
+                if (map[i][k] != c) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    static boolean chekX(int i, int j, char c) {
+        if (i + DOT_TO_WIN - 1 < SIZE) {
+            for (int k = i; k < i + DOT_TO_WIN; k++) {
+                if (map[k][j] != c) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    static boolean chekDiagonal1(int i, int j, char c) {
+        int l = j;
+        if (i + DOT_TO_WIN - 1 < SIZE && j + DOT_TO_WIN - 1 < SIZE) {
+            for (int k = i; k < i + DOT_TO_WIN; k++) {
+                if (map[k][l] != c) {
+                    return false;
+                }
+                l++;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    static boolean chekDiagonal2(int i, int j, char c) {
+        int l = j;
+        if (i - DOT_TO_WIN + 1 >= 0 && j + DOT_TO_WIN - 1 < SIZE) {
+            for (int k = i; k > i - DOT_TO_WIN; k--) {
+                if (map[k][l] != c) {
+                    return false;
+                }
+                l++;
+            }
+            return true;
+        }
+        return false;
+    }
 }

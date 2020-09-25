79747ea50017825da80c00abc7fac5ec1ad707ba
Hroar6 <romaartemyev@gmail.com>
2020-09-25 20:13:48 +0300

    homework 2

--- src/com/company/Actions.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface Actions {
-    boolean run(int length);
-    boolean jump(int height);
-}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,44 @@
 package com.company;
 
-import com.company.enums.DayOfWeek;
-
 public class Main {
     public static void main(String[] args) {
-        Actions[] members = {
-                new Cat(1500, 5),
-                new Human(2000, 2),
-                new Robot(10000, 1)
+        String[][] w = {
+                {"5", "3", "1", "1"},
+                {"3", "3", "2", "1"},
+                {"6", "0", "2", "1"},
+                {"2", "10", "3", "5"}
         };
 
-        Obstacle[] obstacles = {
-                new RunningTrack(1000),
-                new Wall(2),
-                new RunningTrack(1900),
-                new Wall(5)
-        };
+        try {
+            System.out.println(arrCheckSize(w));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
 
-        for (int j = 0; j < members.length; j++) {
-            for (int i = 0; i < obstacles.length; i++) {
-                if (obstacles[i].test(members[j])) {
-                    System.out.println("Участник под номером " + (j + 1) + " прошел " + (i + 1) + " препятствие");
-                } else {
-                    System.out.println("Участник под номером " + (j + 1) + " выбыл");
-                    break;
-                }
+    }
 
+    static int arrCheckSize(String[][] arr) throws MyArrayDataException, MyArraySizeException {
+        int sum = 0;
+        int arrLength = 4;
+        if (arr.length != arrLength) {
+            throw new MyArraySizeException("Количество строк не равно " + arrLength);
+        }
+        for (int i = 0; i < arrLength; i++) {
+            if (arr[i].length != arrLength) {
+                throw new MyArraySizeException("Количество столбов в " + i + " строке не равено " + arrLength);
             }
         }
+//        System.out.println("Массив прошел проверочку");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Не возможно преобразовать [" + i + "][" + j + "] элемент");
+                }
+            }
+        }
+        return sum;
     }
 }
